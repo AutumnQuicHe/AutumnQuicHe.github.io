62f3d0ef4945bab8f2b9da9bebd4a42f9e2b3c45
fangqiuhang <fangqiuhang@163.com>
2022-07-15 22:40:08 +0800

    update sass

--- build.sh
+++ build.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+clear_public() {
+  DIRS="404.html
+    categories
+    css
+    index.html
+    index.xml
+    QUIC
+    RFC8999_Chinese_Translation
+    RFC9000_Chinese_Translation
+    rfc9000-zh-cn
+    RFC9001_Chinese_Translation
+    RFC9002_Chinese_Translation
+    RFC9114_Chinese_Simplified
+    RFC9204_Chinese_Simplified
+    RFC9221_Chinese_Simplified
+    RFC9250_Chinese_Simplified
+    sitemap.xml
+    tags
+    test
+    Translation_Norms"
+
+  for x in ${DIRS}; do
+    rm -rf public/$x
+  done
+}
+
+clear_public
+
 ./scripts/sass.py
 hugo
 find public -name "*.scss" | xargs rm
